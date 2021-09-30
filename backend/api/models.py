dccf924c72789ed2ea95239b580143adbc32277d
keplian <keplian@gmail.com>
2021-09-30 18:00:52 +0300

    start wrote some models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredients(models.Model):
+    name = models.CharField(verbose_name=_("Ingredient's name"), max_length=200)
+    measurement_unit = models.CharField(verbose_name=_("Measurement unit"))
+
+
+class Tags(models.Model):
+    name = models.CharField(verbose_name=_("Tag's name"), max_length=200)
+    color = models.CharField(verbose_name=_('HEX cod for tag color'),
+                             max_length=7)
+    slug = models.SlugField(verbose_name=_('Slug'), max_length=200)
+
+
+class Recipes(models.Model):
+    ingredients = models.ManyToManyField(Ingredients)
+    author = models.ForeignKey(User, verbose_name=_("Recipe's author"),
+                               on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tags, verbose_name=_("Recipe's tags"),
+                                  blank=True)
+    image = models.ImageField(verbose_name=_("Recipe's image"))
+    name = models.CharField(verbose_name="Recipe's name", max_length=200)
+    text = models.TextField(verbose_name="Recipe's description")
+    cooking_time = models.IntegerField(
+        verbose_name=_("Time for cooking (in minutes)"),
+        validators=[MinValueValidator(1)])
+
+
+class Subscribe(models.Model):
+    pass
+
+
+class Favorites:
+    pass
+
+
+class ShopingCard(models.Model):
+    pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-class 
+
+class Users(AbstractUser):
+    """Custom User model."""
+
+    class Roles(models.TextChoices):
+        """Custom roles for use custom permissions in api"""
+        ADMIN = "admin", _("Administrator")
+        USER = "user", _("Moderator")
+
+    email = models.EmailField(_("email address"), unique=True)
+    role = models.CharField(_("role"), choices=Roles.choices,
+                            default=Roles.USER, max_length=30)
+    first_name = models.CharField(_('first name'), max_length=150, unique=True)
+    last_name = models.CharField(_('last name'), max_length=150, unique=True)
+
+    @property
+    def is_admin(self):
+        if self.role == "admin" or self.is_superuser:
+            return True
+
+    @property
+    def is_user(self):
+        if self.role == "user":
+            return True
+
+    class Meta:
+        ordering = ("username",)
+
+
+
+
