867acedb4d6b7eedd108bab63a19d5049d3fe255
keplian <keplian@gmail.com>
2021-10-01 17:26:12 +0300

    fix some isues in models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
@@ -8,14 +9,25 @@ class Users(AbstractUser):
 
     class Roles(models.TextChoices):
         """Custom roles for use custom permissions in api"""
+
         ADMIN = "admin", _("Administrator")
         USER = "user", _("Moderator")
 
     email = models.EmailField(_("email address"), unique=True)
-    role = models.CharField(_("role"), choices=Roles.choices,
-                            default=Roles.USER, max_length=30)
-    first_name = models.CharField(_('first name'), max_length=150, unique=True)
-    last_name = models.CharField(_('last name'), max_length=150, unique=True)
+    role = models.CharField(
+        _("role"), choices=Roles.choices, default=Roles.USER, max_length=30
+    )
+    username = models.CharField(
+        _("username"),
+        unique=True,
+        max_length=150,
+        validators=[RegexValidator(regex=r"^[\w.@+-]+\z")],
+        error_messages={
+            "unique": _("A user with that username already " "exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150, unique=True)
+    last_name = models.CharField(_("last name"), max_length=150, unique=True)
 
     @property
     def is_admin(self):
@@ -29,7 +41,3 @@ class Users(AbstractUser):
 
     class Meta:
         ordering = ("username",)
-
-
-
-
