e98e06e615df72ec99fd8d1b3f72b2ace70a9041
keplian <keplian@gmail.com>
2021-10-20 12:40:30 +0300

    add wait scritp for bd

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,6 +5,7 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
+sleep 5
 python manage.py makemigrations
 python manage.py migrate
 
