7bfe50d2242dd2ef635f839a6e10b85a8f88f2cd
keplian <keplian@gmail.com>
2021-10-21 10:06:39 +0300

    try to fix problem with waiting db container

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-set -e
-
-host="db"
-port="5432"
-
->&2 echo "!!!!!!!! Check conteiner_a for available !!!!!!!!"
-
-until curl -s "$host":"$port"; do
-  >&2 echo "Postgre is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgre is up - executing command"
+# Section to future implement waitng db coonection
+#set -e
+#
+#host="db"
+#port="5432"
+#
+#>&2 echo "!!!!!!!! Check conteiner_a for available !!!!!!!!"
+#
+#until curl -s "$host":"$port"; do
+#  >&2 echo "Postgre is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#>&2 echo "Postgre is up - executing command"
 
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
+sleep 5
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations
